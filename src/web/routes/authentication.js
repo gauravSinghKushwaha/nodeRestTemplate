d80c53970d57fdf73b5fac4e4263ca34313ea590
Gaurav Singh. Kushwaha <gkushwah@oradev.oraclecorp.com>
2017-07-13 16:07:01 +0530

    bug fixes

--- src/web/routes/authentication.js
+++ src/web/routes/authentication.js
@@ -16,10 +16,10 @@ const router = express.Router();
 router.use(function timeLog(req, res, next) {
     var credentials = auth(req);
     if (!credentials || credentials.name != config.apiauth.user || credentials.pass != config.apiauth.pwd) {
-        log.warn('access denied for credentials = ' + credentials.name + ' pwd = ' + credentials.pass);
+        log.warn('basic auth failed, access denied for api . credentials = ' + credentials);
         res.status(401);
         res.setHeader('WWW-Authenticate', 'Basic realm="Secure Area"');
-        return res.send('{success:Access denied}');
+        return res.send({msg: 'Access denied'});
     }
     next();
 });
@@ -45,18 +45,18 @@ router.route('/authenticate').post(function (req, res) {
     restclient.getReq(undefined, config.psurl, arg(body.username), function (err, data, resp) {
         if (err) {
             log.error(err);
-            return res.status(500).send('something wrong at server');
+            return res.status(500).send({error: 'something gone wrong at server'});
         }
         log.debug('response statusCode : ' + resp.statusCode);
         if (resp.statusCode != 200) {
             if (data.hasOwnProperty("error")) {
                 return res.send(data.error);
             } else {
-                return res.status(resp.statusCode).send({'error': 'could not fetch data from persistence server!!'});
+                return res.status(resp.statusCode).send({error: 'could not fetch data from persistence server!!'});
             }
         } else {
             if (data && data.length == 1 && data[0].username == req.body.username && data[0].status == 'active' && data[0].domain == req.body.domain && data[0].password == hash) {
-                return res.status(200).send('{success:true}');
+                return res.status(200).send({success: true});
             } else {
                 log.debug('authentication failed');
                 return res.status(401).send();
